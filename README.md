f766be865958adf2c7ddb7f4636df3bfb2bc83ef
Alex Rodriguez <alex.rodriguez.22@dartmouth.edu>
2021-01-05 22:33:40 -0500

    adding graphics and grow function

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Localization Project
 
 Current status: 
 
-I am currently trying to figure out how the grow functino is meant to be set up
-Without the grow function, it seems that I never actually change any of the intersections
+I am getting an issue where my return values are 2 points when it should be an area, additionally, neither point is close to the node I am trying to localize. 
+To assit, I have made simple graphics 
+next, I am going to try to figure out why the return value is two points. That seems like the easiest problem to tackle, I just need to run the algorthim until I see my mistake
--- localization.py
+++ localization.py
@@ -1,5 +1,9 @@
 # imports
 from sympy import *
+import os
+# suppress message
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame as py
 #---------------------------
 
 
@@ -9,15 +13,25 @@ mode = 'idle'
 mobile_nodes = []
 und_node = []
 areas = []
-point_radius=2
-radius = 12
+point_radius=5
+radius = 120
 max_speed = 2
+screen_size=(500, 600)
 ############
+
+# load functions
+py.init()
+screen = py.display.set_mode(screen_size)
+white = (255, 255, 255)
+black = (0, 0, 0)
+screen.fill(white)
+####
 
 def create_node(table, x, y):
   curr_node = Point2D(x, y)
   table.append(curr_node)
-
+  py.draw.circle(screen, black, (x*10, y*10), point_radius)
+  py.display.flip()
 
   return curr_node
 
@@ -30,14 +44,17 @@ z = create_node(mobile_nodes, 30, 20)
 
 for node in range(len(mobile_nodes)):
   areas.append(Circle(mobile_nodes[node], radius))
-
+  py.draw.circle(screen, black, (mobile_nodes[node].x*10, mobile_nodes[node].y*10), radius, 1)
+  py.display.flip()
 # CHECK TO MAKE SURE IT IS SET UP PROPERLY
 #for area in range(len(areas)):
 #  print(areas[area])
 
-def grow(b):
-	# b is an area
-	return b 
+def grow(old_circle, sbydelta):
+	new_circle = Circle(old_circle.center, old_circle.radius+sbydelta)
+	#new_circle = Circle((old_circle.center.x*10, old_circle.center.y)*10, old_circle.radius+sbydelta)
+
+	return new_circle
 
 
 # Algorithm
@@ -52,17 +69,25 @@ def localize(areas):
 		# set delta_t
 		delta_t = k - (k-1)
 		# the intersection of k is Ak
-		I[k] = intersection(areas[k], grow(I[k-1]))
+		I[k] = intersection(areas[k], grow(I[k-1], s))
 		# loop from j = k - 1 to 1
 		for j in range(k-1, 0, -1):
 			# delta_t is j - (j-1)
 			delta_t = j - (j - 1)
 			# the intersction of j is Aj
-			Ij[j] = intersection(areas[j], grow(I[j-1]))
+			Ij[j] = intersection(areas[j], grow(I[j-1], s))
 	return I[-1]
 
 a = localize(areas)
 
+print(a)
+
+
+running = True
+while running:
+  for event in py.event.get():
+    if event.type == py.QUIT:
+      running = False
 
 
 # doesn't work, maybe bc missing grow? 
