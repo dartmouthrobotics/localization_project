6175881d29602db562c4d7e8432ba058fe13ec80
Alex Rodriguez <alex.rodriguez.22@dartmouth.edu>
2021-02-14 17:44:24 -0500

    algorithm works:no tests

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Localization Project
 
 
 Current status: 
-
-I am getting an issue where my return values are 2 points when it should be an area, additionally, neither point is close to the node I am trying to localize. 
-To assit, I have made simple graphics 
-next, I am going to try to figure out why the return value is two points. That seems like the easiest problem to tackle, I just need to run the algorthim until I see my mistake
+The program seems to be calculating the intersection of the three points well. Some tests I need to run to make sure it works:
+	- Move the points around more
+		- with constant radius
+		- with varying radii
+	- Try with different number of nodes
--- localization.py
+++ localization.py
@@ -1,9 +1,9 @@
 # imports
-from sympy import *
 import os
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+
 # suppress message
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame as py
 #---------------------------
 
 
@@ -14,47 +14,41 @@ mobile_nodes = []
 und_node = []
 areas = []
 point_radius=5
-radius = 120
+radius = 20
 max_speed = 2
-screen_size=(500, 600)
+screen_size=(1000, 1000)
 ############
 
 # load functions
-py.init()
-screen = py.display.set_mode(screen_size)
 white = (255, 255, 255)
 black = (0, 0, 0)
-screen.fill(white)
+figure, axes = plt.subplots()
+axes.axis("equal")
 ####
 
+# --- Creates node --- #
 def create_node(table, x, y):
-  curr_node = Point2D(x, y)
+  curr_node = Point(x, y)
   table.append(curr_node)
-  py.draw.circle(screen, black, (x*10, y*10), point_radius)
-  py.display.flip()
+  plt.plot(x, y, 'o')
 
   return curr_node
 
-# Set up space
-  
+# Set up space  
 w = create_node(mobile_nodes, 10, 20)
-z = create_node(und_node, 20, 15)
-y = create_node(mobile_nodes, 20, 10)
-z = create_node(mobile_nodes, 30, 20)
+y = create_node(mobile_nodes, 30, 10)
+x = create_node(mobile_nodes, 30, 20)
 
 for node in range(len(mobile_nodes)):
-  areas.append(Circle(mobile_nodes[node], radius))
-  py.draw.circle(screen, black, (mobile_nodes[node].x*10, mobile_nodes[node].y*10), radius, 1)
-  py.display.flip()
-# CHECK TO MAKE SURE IT IS SET UP PROPERLY
-#for area in range(len(areas)):
-#  print(areas[area])
+  areas.append(mobile_nodes[node].buffer(radius))
+  axes.add_patch(plt.Circle((mobile_nodes[node].x, mobile_nodes[node].y), radius, color='black', fill=False))
+#############
 
+
+# --- For now, returns old circle --- #
 def grow(old_circle, sbydelta):
-	new_circle = Circle(old_circle.center, old_circle.radius+sbydelta)
 	#new_circle = Circle((old_circle.center.x*10, old_circle.center.y)*10, old_circle.radius+sbydelta)
-
-	return new_circle
+	return old_circle
 
 
 # Algorithm
@@ -65,29 +59,28 @@ def localize(areas):
 	I = [areas[0], areas[1], areas[2]]
 	Ij = [areas[0], areas[1], areas[2]]
 	# loop from 2 to number of intersections
-	for k in range(2, len(I), 1):
+	for k in range(1, len(I), 1): # only goes once
 		# set delta_t
 		delta_t = k - (k-1)
 		# the intersection of k is Ak
-		I[k] = intersection(areas[k], grow(I[k-1], s))
+		#I[k] = intersection(areas[k], grow(I[k-1], s))
+		I[k] = areas[k].intersection(grow(I[k-1], s))
 		# loop from j = k - 1 to 1
 		for j in range(k-1, 0, -1):
 			# delta_t is j - (j-1)
 			delta_t = j - (j - 1)
 			# the intersction of j is Aj
-			Ij[j] = intersection(areas[j], grow(I[j-1], s))
+			#Ij[j] = intersection(areas[j], grow(I[j-1], s))
+			Ij[j] = areas[j].intersection(grow(I[j-1], s))
 	return I[-1]
 
 a = localize(areas)
 
-print(a)
-
-
-running = True
-while running:
-  for event in py.event.get():
-    if event.type == py.QUIT:
-      running = False
+# plotting
+x,y = a.exterior.xy
+plt.plot(x,y)
+plt.fill_between(x, y, 0, color="red", alpha=0.2)
+plt.show()
 
 
 # doesn't work, maybe bc missing grow? 
